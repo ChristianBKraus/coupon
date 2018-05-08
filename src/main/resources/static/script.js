77ce06ac2924ea2931aadec524198116dc30e14b
Christian Kraus <Christian.Kraus@sap.com>
2018-05-08 19:03:35 +0200

    First Version of AdminSite + Authorization

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,37 +1,105 @@
-var app = angular.module('Demo', []);
+var app = angular.module('Coupon', ['ngCookies','base64']);
 
-app.controller('Ctrl', function($scope, $http) {
+app.controller('Ctrl', function($scope, $http, $cookies, $window, $base64) {
 
-	$scope.personen = [];
-	$scope.new_person = {};
-	$scope.test = { name: "Hello" };
+	$scope.ines = {};
+	$scope.jonathan = {};
 			 			 			 
-	 $scope.readPersonen = function() {
-		 console.log("Lesen von Personen");
-	     $http.get('/api/person' )
-			  .then(  function(response) {
-			  		console.log("Personen wurden gelesen");
-			    	$scope.personen = response.data;
-					console.log("Antwort: ");
-					$scope.personen.forEach(function(el){
-						console.log(el);
-					});
-					console.log( $scope.personen );
-			  });
-	};
-	$scope.readPersonen();
+	$scope.readInes = function() {
+		console.log("Read Balances Ines");
+		$http.get('/coupon/balance/ines' )
+			 .then(  function(response) {
+				   	console.log("Balance Ines read");
+				   	$scope.ines = response.data;
+				   	console.log("Response: " + $scope.ines );
+			 });
+    };
 
-    $scope.add = function() {
-		console.log("Hinzufuegen von Person");
-		$http.post('api/person')
+    $scope.readJonathan = function() {
+		console.log("Read Balances Joni");
+		$http.get('/coupon/balance/jonathan' )
+		    .then(  function(response) {
+				 	console.log("Balances Jonathan read");
+			   		$scope.jonathan = response.data;
+			   		console.log("Response: " + $scope.jonathan);
+		 });
+	};
+
+	$scope.authorize = function() {
+		var token = "";
+
+		// Get Password from URL
+		var url = $window.location.href;
+		console.log("URL: " + url);
+		var qparams = url.split('?');
+		if (qparams.length > 1) {
+			var params = new URLSearchParams(qparams[1]);
+			var value = params.get("password");	
+			console.log("QParam[Password] = " + value);
+			token = "Basic " + $base64.encode("ADMIN" + ':' + value);
+		}
+
+		// Get Password from current Authorization Header
+		if (token === "") {
+			var value = $http.defaults.headers.common["Authorization"];
+			console.log("Authorization Header: " + value);
+			if (value != "" && typeof value != 'undefined') {
+				token = value;
+			}
+		} 
+
+		// Get Password from Cookies
+		if (token === "") {
+			var value = $cookies.get('coupon_auth');
+			console.log("Stored Cookie: " + value);
+			if (value != "" && typeof value != 'undefined') {
+				token = value;
+			}
+		}
+
+		// Save Password and Set Authorization
+		if (token != "") {
+			var exp_date = new Date(3000,12,31);
+			$cookies.put('coupon_auth', token, { expires: exp_date });
+			console.log("Setting Cookie: " + token);
+			console.log("Cookie: " + $cookies.get('coupon_auth'));
+
+			$http.defaults.headers.common['Authorization'] = token;
+			return true;
+		} else {
+			console.log("Cookie: " + $cookies.get('coupon_auth'));
+			return false;
+		}
+	};
+
+	$scope.read = function() {
+		var authorized = $scope.authorize();
+		if ( authorized === true ) {
+			console.log("Password found --> Read")
+			$scope.readInes();
+			$scope.readJonathan();
+		} else {
+			console.log("No Password --> Login")
+			//$window.location.href = "/login";
+		}
+	}
+	$scope.read();
+
+	$scope.issue = function(name) {
+		console.log("Issue Coupon for " + name);
+		url = 'coupon/issue/' + name + '/25';
+		console.log("URL: " + url);
+		$http.post(url)
 		     .then( function success(response) {
-		    			console.log("Person hinzugefuegt");
-		    			$scope.readPersonen();
+		    			console.log("Coupon issued");
+						$scope.readInes();
+						$scope.readJonathan();
 		    		}, function error(response) {
-		    	      	console.log("Fehler beim hinzufuegen von Personen");
+		    	      	console.log("Error while issuing coupon");
 		    	      	response.data.errors.forEach(function(e) {
 		    	    	  	console.log(e);
 		    	      	})
 		    	   });
 	};
+
 });		
